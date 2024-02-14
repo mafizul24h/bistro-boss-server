974fd1d28da4ac88a7bcc476a621ccbd98415dd3
mafizul24h <mafizul24h@gmail.com>
2024-02-14 22:34:53 +0600

    Cart Update

--- index.js
+++ index.js
@@ -23,25 +23,44 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const menuCollections = client.db('bistroBD').collection('menu');
+    const cartCollections = client.db('bistroBD').collection('carts');
     const reviewCollections = client.db('bistroBD').collection('reviews');
     const recommendCollections = client.db('bistroBD').collection('recommend');
 
-    app.get('/menu', async(req, res) => {
-        const result = await menuCollections.find().toArray();
-        res.send(result);
+    app.get('/menu', async (req, res) => {
+      const result = await menuCollections.find().toArray();
+      res.send(result);
     })
 
-    app.get('/reviews', async(req, res) => {
-        const result = await reviewCollections.find().toArray();
-        res.send(result);
+    app.get('/reviews', async (req, res) => {
+      const result = await reviewCollections.find().toArray();
+      res.send(result);
     })
 
-    app.get('/recommends', async(req, res) => {
-        const result = await recommendCollections.find().toArray();
+    app.get('/recommends', async (req, res) => {
+      const result = await recommendCollections.find().toArray();
+      res.send(result);
+    })
+
+    app.get('/carts', async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      if (!email) {
+        res.send([]);
+      } else {
+        const result = await cartCollections.find(query).sort({entryDate: -1}).toArray();
         res.send(result);
+      }
+    })
+
+    app.post('/carts', async (req, res) => {
+      const item = req.body;
+      item.entryDate = new Date();
+      const result = await cartCollections.insertOne(item);
+      res.send(result);
     })
 
     // Send a ping to confirm a successful connection
@@ -56,9 +75,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Bistro Boss Server Running');
+  res.send('Bistro Boss Server Running');
 })
 
 app.listen(port, () => {
-    console.log(`Bistro Boss Server Running Port ${port}`);
+  console.log(`Bistro Boss Server Running Port ${port}`);
 })
