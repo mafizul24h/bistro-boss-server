dc64a3717ae2dc1bd6a44e41558de95105734c31
mafizul24h <mafizul24h@gmail.com>
2024-02-17 23:16:49 +0600

    Delete Carts

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -60,6 +60,13 @@ async function run() {
       const item = req.body;
       item.entryDate = new Date();
       const result = await cartCollections.insertOne(item);
+      res.send(result);
+    })
+
+    app.delete('/carts/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)};
+      const result = await cartCollections.deleteOne(query);
       res.send(result);
     })
 
